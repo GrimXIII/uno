3e9286dcc7258699b5c51a194803ca90824d6ae1
GrimXIII <92393831+GrimXIII@users.noreply.github.com>
2022-01-15 23:40:20 -0600

    Update Homepage.js

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -11,9 +11,9 @@ const Homepage = () => {
                 <img src={require('../assets/logo.png').default} width='200px' />
                 <div className='homepage-form'>
                     <div className='homepage-join'>
-                        <Link to={`/play?roomCode="Lobby 1"><button className="game-button green">Lobby 1</button></Link>
-                        <Link to={`/play?roomCode="Lobby 2"><button className="game-button green">Lobby 2</button></Link>
-                        <Link to={`/play?roomCode="Lobby 3"><button className="game-button green">Lobby 3</button></Link>
+                        <Link to={`/play?roomCode="Lobby 1"`><button className="game-button green">Lobby 1</button></Link>
+                        <Link to={`/play?roomCode="Lobby 2"`><button className="game-button green">Lobby 2</button></Link>
+                        <Link to={`/play?roomCode="Lobby 3"`><button className="game-button green">Lobby 3</button></Link>
                     </div>
                 </div>
             </div>
